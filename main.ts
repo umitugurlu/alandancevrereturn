fbb9993a36990e63a3e0c9eda6689e47eb67c630
Ümit UĞURLU <umitugurlu02@gmail.com>
2020-05-13 03:06:05 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,8 @@ function alandanKenarBul (alan: number) {
     kenar = Math.sqrt(alan)
     return kenar
 }
-let kenar = 0
 let cevre = 0
+let kenar = 0
+kenar = 0
+cevre = 0
 basic.showNumber(CevreHesapla(alandanKenarBul(36)))
